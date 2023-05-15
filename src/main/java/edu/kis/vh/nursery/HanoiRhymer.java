c042b41214c8c5460ac7b1d469861621b5c4e85d
RzymonPawlik <97476759+RzymonPawlik@users.noreply.github.com>
2023-05-15 15:16:50 +0200

    17. Wygeneruj automatycznie szkielet dokumentacji do klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -5,9 +5,17 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
     public static final int INT2 = 0;
     private int totalRejected = INT2;
 
+    /**
+     *
+     * @return totalRejected
+     */
     protected int reportRejected() {
         return totalRejected;
     }
+    /**
+     * funkcja liczy ile razzy został spełniony warunek
+     * @param in liczba podlegająca zwiększeniu
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
